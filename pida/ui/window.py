84ebab8a0e894e3da6ff9b54ef3206ec2a9669bc
aafshar <devnull@localhost>
2007-03-08 16:23:04 +0000

    added a boss and test fixture

---
+++ pida/ui/window.py
@@ -0,0 +1,46 @@
+import gtk
+
+from kiwi.ui.delegates import GladeDelegate
+from kiwi.ui.dialogs import save, open as opendlg, info, error, yesno, get_input
+
+
+class MainGladeDelegate(GladeDelegate):
+
+    def __init__(self, app, *args, **kw):
+        self._app = app
+        GladeDelegate.__init__(self, delete_handler=self._on_delete_event, *args, **kw)
+        self.create_all()
+        self.show()
+
+    def _on_delete_event(self, window, event):
+        if self.yesno_dlg('Are you sure you want to exit?'):
+            self.hide_and_quit()
+        else:
+            return True
+
+    def create_all(self):
+        pass
+
+    # Dialogs
+    def save_dlg(self, *args, **kw):
+        return save(parent = self.get_toplevel(), *args, **kw)
+
+    def open_dlg(self, *args, **kw):
+        return opendlg(parent = self.get_toplevel(), *args, **kw)
+
+    def info_dlg(self, *args, **kw):
+        return info(parent = self.get_toplevel(), *args, **kw)
+
+    def error_dlg(self, *args, **kw):
+        return error(parent = self.get_toplevel(), *args, **kw)
+
+    def yesno_dlg(self, *args, **kw):
+        return yesno(parent = self.get_toplevel(), *args, **kw) == gtk.RESPONSE_YES
+
+    def error_list_dlg(self, msg, errs):
+        return self.error_dlg('%s\n\n* %s' % (msg, '\n\n* '.join(errs)))
+
+    def input_dlg(self, *args, **kw):
+        return get_input(parent=self.get_toplevel(), *args, **kw)
+
+
