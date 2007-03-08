1ff57093a5d8cc059bebb52e8e9f91c76e625116
aafshar <devnull@localhost>
2007-03-08 17:40:02 +0000

    some testing methods

--- pida/utils/testing.py
+++ pida/utils/testing.py
@@ -1,8 +1,13 @@
 """ Some things to make testing the UI a bit nicer """
+
+
+# stlib
 import time
 
+# gtk
 import gtk
 
+# Stolen from Kiwi
 def refresh_gui(delay=0):
     while gtk.events_pending():
         gtk.main_iteration_do(block=False)
