578a0b0f87a95d0d2c56014acb3f0debdec4f78b
aafshar <devnull@localhost>
2007-03-12 17:25:04 +0000

    refactored servicecreator and gave it some options

--- tools/servicecreator.py
+++ tools/servicecreator.py
@@ -8,7 +8,7 @@ def log(txt):
 
 python_module_template = '''# -*- coding: utf-8 -*- 
 
-# Copyright (c) %(year)s The PIDA Project
+# Copyright (c) %(year)s %(copyright_holder)s
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
@@ -28,20 +28,62 @@ python_module_template = '''# -*- coding: utf-8 -*-
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-# System import(s)
+%(system_imports)s
+%(gtk_imports)s
+%(library_imports)s
+%(pida_imports)s
 
-# GTK import(s)
-
-# Library import(s)
-
-# PIDA import(s)
 %(content)s
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
 '''
 
-plugin_template = '''from pida.core.service import Service
 
+def create_python_file(root_path, name, content,
+                       copyright_holder='The PIDA Project',
+                       s_imports=[],
+                       g_imports=[],
+                       l_imports=[],
+                       p_imports=[]):
+    if not name.endswith('.py'):
+        name = '%s.py' % name
+
+    path = os.path.join(root_path, name)
+    f = open(path, 'w')
+
+    if s_imports:
+        s_imports.insert(0, '# Standard Library Imports')
+    sys_imports = '\n'.join(s_imports)
+
+    if g_imports:
+        g_imports.insert(0, '# GTK Imports')
+    gtk_imports = '\n'.join(g_imports)
+
+    if l_imports:
+        l_imports.insert(0, '# Other Imports')
+    library_imports = '\n'.join(l_imports)
+
+    if p_imports:
+        p_imports.insert(0, '# PIDA Imports')
+    pida_imports = '\n'.join(p_imports)
+
+    data = python_module_template % dict(
+            content=content,
+            copyright_holder=copyright_holder,
+            system_imports=sys_imports,
+            gtk_imports=gtk_imports,
+            library_imports=library_imports,
+            pida_imports=pida_imports,
+            year=date.today().year,
+    )
+
+    f.write(data)
+    f.close()
+
+
+class ServiceCreator(object):
+
+    service_template = '''
 # Service class
 class %(name)s(Service):
     """Describe your Service Here""" 
@@ -51,71 +93,77 @@ Service = %(name)s
 
 '''
 
-def create_plugin_dir(root_path, name):
-    if not os.path.isdir(root_path):
-        raise ValueError('The root path is not a directory')
-    path = os.path.abspath(os.path.join(root_path, name))
-    if os.path.exists(path):
-        raise ValueError('The plugin directory already exists')
-    else:
-        os.mkdir(path)
-        return path
-
-def create_plugin_module(path, name):
-    modname = '%s.py' % name
-    modpath = os.path.join(path, modname)
-    f = open(modpath, 'w')
-    content = plugin_template % dict(name=modname.capitalize())
-    data = python_module_template % dict(
-        content=content,
-        year = date.today().year
+    service_template_imports = dict(
+        p_imports = [
+            'from pida.core.service import Service',
+        ]
     )
-    f.write(data)
-    f.close()
-    initpath = os.path.join(path, '__init__.py')
-    f = open(initpath, 'w')
-    f.close()
+    
+    def __init__(self, root_path, name, opts=None):
+        self._name = name
+        self._root_path = root_path
+        self._path = os.path.abspath(os.path.join(self._root_path, self._name))
 
-def create_servicefile(path):
-    servicefile_path = os.path.join(path, 'service.pida')
-    f = open(servicefile_path, 'w')
-    f.close()
+    def _create_service_text(self):
+        return self.service_template % dict(name=self._name.capitalize())
 
-def create_resource_dirs(path):
-    for res in ['glade', 'data', 'uidef', 'pixmaps']:
-        respath = os.path.join(path, res)
-        os.mkdir(respath)
+    def _create_service_dir(self):
+        if not os.path.isdir(self._root_path):
+            raise ValueError('The root path is not a directory')
+        if os.path.exists(self._path):
+            raise ValueError('The plugin directory already exists')
+        else:
+            os.mkdir(self._path)
 
-def create_plugin(root_path, name):
-    log('Making plugin directory in %s' % root_path)
-    path = create_plugin_dir(root_path, name)
-    log('Creating plugin module named %s at %s' % (path, name))
-    create_plugin_module(path, name)
-    log('Creating service.pida in %s' % path)
-    create_servicefile(path)
-    log('Creating Resource directories in %s' % path)
-    create_resource_dirs(path)
-    os.system('svn add %s' % path)
+    def _create_service_module(self):
+        contents = self._create_service_text()
+        create_python_file(self._path, self._name, contents, **self.service_template_imports)
+        create_python_file(self._path, '__init__', '')
+        create_python_file(self._path, 'test_%s' % self._name, '')
 
-def create_module(root_path, name):
-    if not name.endswith('.py'):
-        name = '%s.py' % name
-    path = os.path.join(root_path, name)
-    data = python_module_template % dict(
-            content='',
-            year=date.today().year
-    )
-    log('Creating module %s at %s' % (name, path))
-    f = open(path, 'w')
-    f.write(data)
-    f.close()
-    testname = 'test_%s' % name
-    log('Creating test module %s at %s' % (testname, path))
-    test_path = os.path.join(root_path, testname)
-    f = open(test_path, 'w')
-    f.write(data)
-    f.close()
-    os.system('svn add %s %s' % (path, test_path))
+    def _create_servicefile(self):
+        servicefile_path = os.path.join(self._path, 'service.pida')
+        f = open(servicefile_path, 'w')
+        f.close()
+
+    def _create_resource_dirs(self):
+        for res in ['glade', 'data', 'uidef', 'pixmaps']:
+            respath = os.path.join(self._path, res)
+            os.mkdir(respath)
+
+    def _add_svn(self):
+        os.system('svn add %s' % self._path)
+
+    def create(self):
+        log('Creating service %s in %s' % (self._name, self._root_path))
+        self._create_service_dir()
+        self._create_service_module()
+        self._create_servicefile()
+        self._create_resource_dirs()
+
+
+class ModuleCreator(object):
+    
+    def __init__(self, root_path, name, opts=None):
+        self._name = name
+        self._root_path = root_path
+
+    def _create_module(self):
+        create_python_file(self._root_path, self._name, '')
+
+    def _create_test_module(self):
+        create_python_file(self._root_path,
+            'test_%s' % self._name,
+            '',
+            s_imports=[
+                'from unittest import TestCase'
+            ]
+        )
+
+    def create(self):
+        self._create_module()
+        self._create_test_module()
+
 
 def get_service_details():
     defpath = os.path.join(os.getcwd(), 'pida', 'services')
@@ -137,21 +185,32 @@ def get_module_details():
         name = raw_input('Please enter the module name: ')
     return path, name.lower()
 
-def main(argv):
-    if len(argv):
-        act = argv[0]
-    else:
-        act = 'module'
-    if act == 'module':
+
+def prime_parser():
+    usage = "usage: %prog [options] module|m|service|s"
+    parser = OptionParser(usage=usage)
+    parser.add_option('-n', '--no-svn',
+        help='Do not add the creation to the subversion repo',
+        dest='add_svn',
+        action='store_false',
+        default=True)
+    return parser
+
+def main():
+    parser = prime_parser()
+    opts, args = parser.parse_args()
+    if not args:
+        parser.error('You must provide an action')
+    act = args[0]
+    if act in ['module', 'm']:
         path, name = get_module_details()
-        create_module(path, name)
-    elif act == 'service':
+        ModuleCreator(path, name, opts).create()
+    elif act in ['service', 's']:
         path, name = get_service_details()
-        create_plugin(path, name)
-
-
-
+        ServiceCreator(path, name, opts).create()
+    else:
+        parser.error('The action must be one of "module", "m", "service", "s"')
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
