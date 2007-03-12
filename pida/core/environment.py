89135574832830d088516cdb372f164cb50b2850
aafshar <devnull@localhost>
2007-03-12 12:56:04 +0000

    moved the data to resources

--- pida/core/environment.py
+++ pida/core/environment.py
@@ -3,8 +3,8 @@ from kiwi.environ import Library, environ
 
 library = Library('pida', root='../..')
 
-library.add_global_resource('glade', 'data/glade')
-library.add_global_resource('uidef', 'data/uidef')
+library.add_global_resource('glade', 'resources/glade')
+library.add_global_resource('uidef', 'resources/uidef')
 
 def get_resource_path(resource, name):
     return environ.find_resource(resource, name)
