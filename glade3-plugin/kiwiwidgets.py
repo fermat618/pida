9f2bde8afe68026fa6a61736ac0b2ef8d8fcccb0
aafshar <devnull@localhost>
2007-04-06 23:30:02 +0000

    added a project manager service

--- glade3-plugin/kiwiwidgets.py
+++ glade3-plugin/kiwiwidgets.py
@@ -5,7 +5,7 @@ import glade
 from kiwi.ui.hyperlink import HyperLink
 from kiwi.ui.objectlist import ObjectList, ObjectTree, Column
 from kiwi.ui.widgets.label import ProxyLabel
-from kiwi.ui.widgets.combo import ProxyComboBoxEntry, ProxyComboBox
+from kiwi.ui.widgets.combo import ProxyComboEntry, ProxyComboBox
 from kiwi.ui.widgets.checkbutton import ProxyCheckButton
 from kiwi.ui.widgets.radiobutton import ProxyRadioButton
 from kiwi.ui.widgets.entry import ProxyEntry, ProxyDateEntry
