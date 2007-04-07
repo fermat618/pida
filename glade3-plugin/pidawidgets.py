382f5022d0d34b0c9237808ab0f4733105440dde
aafshar <devnull@localhost>
2007-04-07 18:18:01 +0000

    pida widget in glade

---
+++ glade3-plugin/pidawidgets.py
@@ -0,0 +1,51 @@
+
+import glade
+import gtk, gobject
+
+import sys
+sys.path.insert(0, '/home/ali/working/pida-next/')
+
+from pida.core import environment
+
+print environment.environ.get_resource_paths('pixmap')
+
+from pida.ui.views import PidaViewWidget
+
+class PidaViewWidgetAdaptor(glade.get_adaptor_for_type('GtkVBox')):
+
+    __gtype_name__ = 'PidaViewWidgetAdaptor'
+
+    def do_post_create(self, pvw, reason):
+        pvw.add_main_widget(gobject.new('GladePlaceholder'))
+
+    def do_replace_child(self, pvw, old, new):
+        pvw.remove_main_widget()
+        pvw.add_main_widget(new)
+
+    def do_get_children(self, pvw):
+        return [pvw.get_main_widget()]
+
+    def do_add(self, pvw, child):
+        pvw.add_main_widget(child)
+
+    def do_child_get_property(self, pvw, child, prop):
+        if prop in ['expand', 'fill']:
+            return True
+        elif prop == 'padding':
+            return 0
+        elif prop == 'position':
+            return 0
+        elif prop == 'pack-type':
+            return gtk.PACK_START
+        return True
+
+    def do_child_set_property(self, pvw, child, prop, val):
+        if prop in ['expand', 'fill', 'padding', 'pack-type']:
+            pass
+
+        
+
+
+
+
+
--- glade3-plugin/setup.py
+++ glade3-plugin/setup.py
@@ -8,12 +8,8 @@ glade3_prefix = '/usr/local'
 setup(
     data_files=[
         (join(glade3_prefix, 'share', 'glade3', 'catalogs'),
-            ('kiwiwidgets.xml',)),
+            ('pidawidgets.xml',)),
         (join(glade3_prefix, 'lib', 'glade3', 'modules'),
-            ('kiwiwidgets.py',)),
-        (join(glade3_prefix, 'share', 'glade3', 'pixmaps', '22x22'),
-            listfiles('..', 'gazpacho-plugin', 'resources', 'kiwiwidgets', '*.png')),
-        (join(glade3_prefix, 'share', 'glade3', 'pixmaps', '16x16'),
-            listfiles('..', 'gazpacho-plugin', 'resources', 'kiwiwidgets', '*.png')),
+            ('pidawidgets.py',)),
     ]
 )
