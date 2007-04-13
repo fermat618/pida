12dbdf69b0a7521002950d4b8b51d53c8b82a72d
aafshar <devnull@localhost>
2007-04-13 12:45:02 +0000

    moo extension

--- setup.py
+++ setup.py
@@ -1,16 +1,35 @@
 
-from distutils.core import setup
 
+import os
+from distutils.core import setup, Extension
+from distutils.command.build_ext import build_ext
+from dsutils import pkc_get_include_dirs, pkc_get_libraries, pkc_get_library_dirs
+  
+moo = Extension('moo_stub', 
+                 ['moo/moopaned.c',
+                  'moo/moobigpaned.c',
+                  'moo/moomarshals.c',
+                  'moo/moo-pygtk.c',
+                  'moo/moo-stub.c',
+                 ],
+                 include_dirs=pkc_get_include_dirs('gtk+-2.0 pygtk-2.0'),
+                 libraries=pkc_get_libraries('gtk+-2.0 pygtk-2.0'),
+                library_dirs=pkc_get_library_dirs('gtk+-2.0 pygtk-2.0'),
+                 )
+ 
+class BuildExt(build_ext):
+    def build_extension(self, ext):
+        if ext.name == 'moo_stub':
+            if os.system('cd moo && make prepare'):
+                raise RuntimeError()
+        build_ext.build_extension(self, ext)
+  
 setup(
-    name='pida',
-    packages = ('pida', 'pida.core', 'pida.ui', 'pida.utils'),
-    data_files = [
-        #('share/gazpacho/catalogs',
-        #    listfiles('gazpacho-plugin', 'pidawidgets.xml')),
-        #('share/gazpacho/resources/pidawidgets',
-        #    listfiles('gazpacho-plugin', 'resources', 'pidawidgets', '*.png')),
-        #(get_site_packages_dir('gazpacho', 'widgets'),
-        #    listfiles('gazpacho-plugin', 'pidawidgets.py')),
-    ]
-
+      name='pida',
+      packages = ('pida',),
+      ext_modules = [moo],
+      data_files = [],
+          #('share/gazpacho/catalogs',
+          #    listfiles('gazpacho-plugin', 'pidawidgets.xml')),
+      cmdclass={'build_ext': BuildExt},
 )
