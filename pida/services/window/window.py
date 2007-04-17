6f886fdc20451def892e1448c0cc4a7cb27cbca9
aafshar <devnull@localhost>
2007-04-17 08:49:00 +0000

    brand new window service

---
+++ pida/services/window/window.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*- 
+
+# Copyright (c) 2007 The PIDA Project
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+
+
+
+# PIDA Imports
+from pida.core.service import Service
+from pida.core.features import FeaturesConfig
+from pida.core.commands import CommandsConfig
+from pida.core.events import EventsConfig
+from pida.core.actions import ActionsConfig
+from pida.core.actions import TYPE_NORMAL, TYPE_MENUTOOL, TYPE_RADIO, TYPE_TOGGLE
+
+
+# Service class
+class Window(Service):
+    """Describe your Service Here""" 
+
+# Required Service attribute for service loading
+Service = Window
+
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
