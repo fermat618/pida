2a6ff2cd218818c66b8b100dc68775d3b1b71159
aafshar <devnull@localhost>
2007-04-21 01:12:05 +0000

    pida remote with proper feedback

--- pida-remote.py
+++ pida-remote.py
@@ -8,13 +8,15 @@ from pida.utils.testing import refresh_gui
 class PidaClientDispatcher(LocalClientDispatcher):
 
     def remote_ok(self, command):
-        pass
+        sys.exit(0)
 
-    def remote_error(self, command):
-        print 'Error'
-
-if __name__ == '__main__':
+def main():
     dispatcher = PidaClientDispatcher(9124)
     file_name = os.path.abspath(sys.argv[-1])
     dispatcher.call_server('open', (file_name,))
     refresh_gui()
+    print 'Error: PIDA was not available'
+    sys.exit(1)
+
+if __name__ == '__main__':
+    main()
