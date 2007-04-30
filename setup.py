7a8fed358bb12a720b1874ed7fe5e21b8cccffed
aafshar <devnull@localhost>
2007-04-30 14:09:02 +0000

    added package metadata to the setup installer

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-
+"""
+The PIDA Installer
+"""
 
 import os
 from distutils.core import setup, Extension
 from distutils.command.build_ext import build_ext
 
+from pida import PIDA_VERSION
+
+
+# Moo Extension
 from moo.dsutils import pkc_get_include_dirs, pkc_get_libraries, pkc_get_library_dirs
-
-from kiwi.dist import listpackages
-
-  
 moo = Extension(
     'moo_stub', 
     [
@@ -30,7 +32,19 @@ class BuildExt(build_ext):
             if os.system('cd moo && make prepare'):
                 raise RuntimeError()
         build_ext.build_extension(self, ext)
-  
+
+
+# Modified from kiwi
+def listpackages(root):
+    packages = []
+    if os.path.exists(os.path.join(root, '__init__.py')):
+        packages.append(root.replace('/', '.'))
+    for filename in os.listdir(root):
+        full = os.path.join(root, filename)
+        if os.path.isdir(full):
+            packages.extend(listpackages(full))
+    return packages
+
 
 def list_pida_packages():
     packages = []
@@ -52,7 +66,7 @@ def list_pida_services(package_data):
     return packages
 
 
-def list_main_data():
+def get_main_data():
     return {
         'pida':
         [
@@ -62,16 +76,22 @@ def list_main_data():
         ]
     }
 
-all_package_data = list_main_data()
+all_package_data = get_main_data()
 
 all_packages = list_pida_packages() + list_pida_services(all_package_data)
 
 setup(
     name='pida',
+    version = PIDA_VERSION,
     packages = all_packages,
     package_data = all_package_data,
     ext_modules = [moo],
     cmdclass={'build_ext': BuildExt},
-    scripts=['bin/pida', 'bin/pida-remote']
+    scripts=['bin/pida', 'bin/pida-remote'],
+    author = 'Ali Afshar',
+    author_email = 'aafshar@gmail.com',
+    url = 'http://pida.co.uk/',
+    download_url = 'http://pida.co.uk/download/',
+    description = 'An intergated development environment that reuses tools such as Vim, and all version control systems.',
 )
 
