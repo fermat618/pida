71d2b193bfc79561a5d4d8db51cd7d2bf7caab57
aafshar <devnull@localhost>
2007-05-02 15:30:00 +0000

    docs: update generation script

--- tools/generate_docs.sh
+++ tools/generate_docs.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-a2x -f chunked -d docs/html/ docs/txt/dev.txt
+a2x -f chunked --asciidoc-opts="-d book" -d docs/html/ docs/txt/dev.txt
 mv docs/html/dev.chunked/* docs/html/dev
 rm -rf docs/html/dev.chunked
