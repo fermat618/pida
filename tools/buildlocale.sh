33690394de0a0cd385a2eee418a62952ae1367c8
txprog <devnull@localhost>
2007-05-03 11:17:05 +0000

    remove pushd/popd

--- tools/buildlocale.sh
+++ tools/buildlocale.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 for file in `find . -iname '*.po' | grep -v 'contrib'`; do
 	echo -n "Processing $file... "
-	pushd . &>/dev/null
-	cd `dirname $file`
-	msgfmt -o `basename $file .po`.mo `basename $file`
-	popd &>/dev/null
+	msgfmt -o `dirname $file`/`basename $file .po`.mo $file
 	echo "OK"
 done
