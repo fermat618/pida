ad0c40d7b0687b7c8cec41d6797db7c1a8dacd43
txprog <devnull@localhost>
2007-05-10 16:39:02 +0000

    Gnu-global integration (#97)

---
+++ pida/services/gtags/gtags.py
@@ -0,0 +1,320 @@
+# -*- coding: utf-8 -*- 
+
+# Copyright (c) 2007 The PIDA Project
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+import os
+import gtk
+import re
+import subprocess
+
+from kiwi.ui.objectlist import ObjectList, Column
+
+# PIDA Imports
+from pida.core.environment import Environment, get_uidef_path
+from pida.core.service import Service
+from pida.core.events import EventsConfig
+from pida.core.actions import ActionsConfig
+from pida.core.actions import TYPE_TOGGLE, TYPE_NORMAL
+from pida.ui.buttons import create_mini_button
+
+from pida.ui.views import PidaView
+
+from pida.utils.gthreads import GeneratorTask, gcall
+
+# locale
+from pida.core.locale import Locale
+locale = Locale('gtags')
+_ = locale.gettext
+
+class GtagsItem(object):
+
+    def __init__(self, file, line, dataline, symbol, search):
+        self.file = file
+        self.line = line
+        self._dataline = dataline
+        self._symbol = symbol
+        self.search = search
+
+        self.symbol = self._color_match(symbol, search)
+        self.dataline = self._color_match(dataline, symbol)
+        self.filename = '%s:<span color="#000099">%s</span>' % (self.file, self.line)
+
+    def _color_match(self, data, match):
+        return data.replace(match, '<span color="#c00000"><b>%s</b></span>' % match)
+
+class GtagsView(PidaView):
+
+    label_text = 'Gtags'
+
+    def create_ui(self):
+        self._hbox = gtk.HBox(spacing=3)
+        self._hbox.set_border_width(2)
+        self.add_main_widget(self._hbox)
+        self._vbox = gtk.VBox(spacing=3)
+        self._hbox.pack_start(self._vbox)
+        self.create_searchbar()
+        self.create_list()
+        self.create_toolbar()
+        self._hbox.show_all()
+
+        self._count = 0
+
+    def create_searchbar(self):
+        h = gtk.HBox(spacing=3)
+        h.set_border_width(2)
+        # label
+        l = gtk.Label()
+        l.set_text(_('Pattern : '))
+        h.pack_start(l, expand=False)
+        # pattern
+        self._search = gtk.Entry()
+        self._search.connect('changed', self._on_search_changed)
+        self._search.set_sensitive(False)
+        h.pack_start(self._search)
+        # info
+        self._info = gtk.Label()
+        self._info.set_text('-')
+        h.pack_start(self._info, expand=False)
+        self._vbox.pack_start(h, expand=False)
+        self._search.show_all()
+
+    def create_toolbar(self):
+        self._bar = gtk.VBox(spacing=1)
+        self._refresh_button = create_mini_button(
+                gtk.STOCK_REFRESH, _('Build TAGS database'),
+                self._on_refresh_button_clicked)
+        self._bar.pack_start(self._refresh_button, expand=False)
+        self._hbox.pack_start(self._bar, expand=False)
+        self._bar.show_all()
+
+    def create_list(self):
+        self._list = ObjectList(
+                [
+                    Column('symbol', data_type=str, title=_('Symbol'),
+                        use_markup=True),
+                    Column('filename', data_type=str, title=_('Location'),
+                        use_markup=True),
+                    Column('dataline', data_type=str, title=_('Data'),
+                        use_markup=True)
+                ]
+        )
+        self._vbox.pack_start(self._list)
+        self._list.show_all()
+
+    def add_item(self, item):
+        self._list.append(item)
+        self._count += 1
+        if self._count == 1:
+            self._info.set_text(_('%d match') % self._count)
+        else:
+            self._info.set_text(_('%d matchs') % self._count)
+
+    def clear_items(self):
+        self._list.clear()
+        self._count = 0
+        self._info.set_text('-')
+
+    def activate(self, activate):
+        self._search.set_sensitive(activate)
+        self._list.set_sensitive(activate)
+
+    def can_be_closed(self):
+        self.svc.get_action('show_gtags').set_active(False)
+
+    def _on_search_changed(self, w):
+        self.svc.tag_search(self._search.get_text())
+
+    def _on_refresh_button_clicked(self, w):
+        self.svc.build_db()
+
+class GtagsActions(ActionsConfig):
+
+    def create_actions(self):
+        self.create_action(
+            'show_gtags',
+            TYPE_TOGGLE,
+            _('Gtags Viewer'),
+            _('Show the gtags'),
+            '',
+            self.on_show_gtags,
+            '',
+        )
+
+        self.create_action(
+            'gtags_current_word_file',
+            TYPE_NORMAL,
+            _('Complete current word'),
+            _('Complete current word'),
+            gtk.STOCK_FIND,
+            self.on_gtags_current_word,
+            '<Control>slash'
+        )
+
+    def on_show_gtags(self, action):
+        if action.get_active():
+            self.svc.show_gtags()
+        else:
+            self.svc.hide_gtags()
+
+    def on_gtags_current_word(self, action):
+        self.svc.tag_search_current_word()
+
+class GtagsEvents(EventsConfig):
+
+    def subscribe_foreign_events(self):
+        self.subscribe_foreign_event('project', 'project_switched',
+                                     self.svc.on_project_switched)
+
+
+# Service class
+class Gtags(Service):
+    """Fetch gtags list and show an gtags"""
+
+    actions_config = GtagsActions
+    events_config = GtagsEvents
+
+    def start(self):
+        self._view = GtagsView(self)
+        self._has_loaded = False
+        self._project = None
+        self._ticket = 0
+
+    def show_gtags(self):
+        self.boss.cmd('window', 'add_view', paned='Terminal', view=self._view)
+        if not self._has_loaded:
+            self._has_loaded = True
+
+    def hide_gtags(self):
+        self.boss.cmd('window', 'remove_view', view=self._view)
+
+    def have_database(self):
+        if self._project is None:
+            return False
+        return os.path.exists(os.path.join(self._project.source_directory, 'GTAGS'))
+
+    def build_db(self):
+        if self._project is None:
+            return False
+        if self.have_database():
+            commandargs = ['gtags']
+        else:
+            commandargs = ['global -u']
+        self._refresh_button.set_sensitive(False)
+        self.svc.boss.cmd('commander', 'execute',
+                commandargs=commandargs,
+                cwd=self._project.source_directory,
+                title=_('Gtags build...'),
+                eof_handler=self.build_db_finished)
+
+    def build_db_finished(self, w):
+        self._view.activate(self.have_database())
+        self._refresh_button.set_sensitive(True)
+
+    def on_project_switched(self, project):
+        if project != self._project:
+            self._project = project
+            self._view.activate(self.have_database())
+
+    def tag_search_current_word(self):
+        self.boss.editor.cmd('call_with_current_word', callback=self.tag_search_cw)
+
+    def tag_search_cw(self, word):
+        self.get_action('show_gtags').set_active(True)
+        self._view._list.grab_focus()
+        self._view._search.set_text(word)
+
+    def tag_search(self, pattern):
+        if not self.have_database() or pattern is None:
+            return
+
+        self._view.clear_items()
+        self._ticket += 1
+
+        self.task = GeneratorTask(self._tag_search, self._view.add_item, None)
+        self.task.start(pattern, self._ticket)
+
+    def _tag_search(self, pattern, ticket):
+        candidates = self._global_complete(pattern)
+        if len(candidates) > 500:
+            return
+
+        if self._ticket != ticket:
+            return
+
+        for candidate in candidates:
+            if candidate == '':
+                continue
+
+            res = self._global_definition(candidate)
+            for entry in res:
+                if entry == '':
+                    continue
+                data = entry.split(':',2)
+                if self._ticket != ticket:
+                    return
+
+                yield GtagsItem(file=data[0], line=data[1],
+                    dataline=data[2], symbol=candidate, search=pattern)
+
+    def _global_complete(self, pattern):
+        commandsargs = [ 'global', '-c', pattern ]
+        p = subprocess.Popen(commandsargs,
+                stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                cwd=self._project.source_directory)
+        data = p.communicate(None)[0]
+
+        return data.split('\n')
+
+    def __global_complete(self, pattern):
+        commandsargs = [ 'global', '-c', pattern ]
+        p = subprocess.Popen(commandsargs,
+                stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                cwd=self._project.source_directory)
+        data = p.communicate(None)[0]
+
+        return data.split('\n')
+
+    def _global_definition(self, symbol):
+        commandsargs = [ 'global', '--result=grep', symbol ]
+        p = subprocess.Popen(commandsargs,
+                stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                cwd=self._project.source_directory)
+        data = p.communicate(None)[0]
+
+        return data.split('\n')
+
+
+    def _global_reference(self, symbol):
+        commandsargs = [ 'global', '--result=grep', '-r', symbol ]
+        p = subprocess.Popen(commandsargs,
+                stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                cwd=self._project.source_directory)
+        data = p.communicate(None)[0]
+
+        return data.split('\n')
+
+
+# Required Service attribute for service loading
+Service = Gtags
+
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
