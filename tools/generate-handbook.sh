17de77a0f99d95433730c380315a00f164ccd9e4
aafshar <devnull@localhost>
2007-07-04 20:24:03 +0000

    updated the html version of the handbook and added a script

---
+++ tools/generate-handbook.sh
@@ -0,0 +1,5 @@
+asciidoc -a icons -a toc -a numbered -a toclevels=4 docs/txt/handbook.txt
+echo "Now:"
+echo "  Change the id=\"header\" to class=\"header\""
+echo "  Move the TOC script inside the body"
+echo "  Copy the toc stylesheet inside the body"
