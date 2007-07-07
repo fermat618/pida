6b073875c65c026afb7fc96d2cdcb2d75ac2f113
david.soulayrol <devnull@localhost>
2007-07-07 19:33:04 +0000

    Cleaned unnecessary code since EditorService.

--- pida/editors/emacs/emacs.py
+++ pida/editors/emacs/emacs.py
@@ -35,135 +35,14 @@ import gobject
 import gtk
 
 # PIDA Imports
-from pida.core.service import Service
-from pida.core.commands import CommandsConfig
-from pida.core.actions import ActionsConfig
-from pida.core.actions import TYPE_NORMAL
-from pida.core.log import build_logger
-from pida.core.editors import EditorService
-
 from pida.ui.views import PidaView
 
-# locale
-from pida.core.locale import Locale
-locale = Locale('pida')
-_ = locale.gettext
+from pida.core.log import build_logger
+from pida.core.editors import EditorService, _
 
 # Emacs specific
 from pida.utils.emacs.emacsembed import EmacsEmbedWidget
 from pida.utils.emacs.emacscom import EmacsClient, EmacsServer, EMACS_SCRIPT
-
-
-class EditorActionsConfig(ActionsConfig):
-
-    def create_actions(self):
-        self.create_action(
-            'undo',
-            TYPE_NORMAL,
-            _('Undo'),
-            _('Undo the last editor action'),
-            gtk.STOCK_UNDO,
-            self.on_undo,
-        )
-
-        self.create_action(
-            'redo',
-            TYPE_NORMAL,
-            _('Redo'),
-            _('Redo the last editor action'),
-            gtk.STOCK_REDO,
-            self.on_redo,
-        )
-
-        self.create_action(
-            'cut',
-            TYPE_NORMAL,
-            _('Cut'),
-            _('Cut the selection in the editor'),
-            gtk.STOCK_CUT,
-            self.on_cut,
-        )
-
-        self.create_action(
-            'copy',
-            TYPE_NORMAL,
-            _('Copy'),
-            _('Copy the selection in the editor'),
-            gtk.STOCK_COPY,
-            self.on_copy,
-        )
-
-        self.create_action(
-            'paste',
-            TYPE_NORMAL,
-            _('Paste'),
-            _('Paste the clipboard in the editor'),
-            gtk.STOCK_PASTE,
-            self.on_paste,
-        )
-
-        self.create_action(
-            'save',
-            TYPE_NORMAL,
-            _('Save'),
-            _('Save the current document'),
-            gtk.STOCK_SAVE,
-            self.on_save,
-        )
-
-    def on_undo(self, action):
-        self.svc.undo()
-
-    def on_redo(self, action):
-        self.svc.redo()
-
-    def on_cut(self, action):
-        self.svc.cut()
-
-    def on_copy(self, action):
-        self.svc.copy()
-
-    def on_paste(self, action):
-        self.svc.paste()
-
-    def on_save(self, action):
-        self.svc.save()
-
-
-class EditorCommandsConfig(CommandsConfig):
-
-    def open(self, document):
-        self.svc.open(document)
-
-    def close(self, document):
-        self.svc.close(document)
-
-    def goto_line(self, line):
-        self.svc.goto_line(line)
-
-    def define_sign_type(self, type, icon, linehl, text, texthl):
-        self.svc.define_sign_type(type, icon, linehl, text, texthl)
-
-    def undefine_sign_type(self, type):
-        self.svc.undefine_sign_type(type)
-
-    def get_current_line_number(self):
-        return self.svc.get_current_line()
-
-    def show_sign(self, type, file_name, line):
-        self.svc.show_sign(type, file_name, line)
-
-    def hide_sign(self, type, file_name, line):
-        self.svc.hide_sign(type, file_name, line)
-
-    def call_with_current_word(self, callback):
-        self.svc.call_with_current_word(callback)
-
-    def call_with_selection(self, callback):
-        self.svc.call_with_selection(callback)
-
-    def grab_focus(self):
-        self.svc.grab_focus()
 
 
 class EmacsView(PidaView):
@@ -265,9 +144,6 @@ class Emacs(EditorService):
     there and orders for Emacs are sent to it which forwards them to the
     EmacsClient instance. 
     """ 
-
-    commands_config = EditorCommandsConfig
-    actions_config = EditorActionsConfig
 
     def _create_initscript(self):
         self.script_path = os.path.join(
