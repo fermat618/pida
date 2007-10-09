8ece36f08491ba5ca4f7410dddc9e37eb89d92ce
ronny.pfannschmidt <devnull@localhost>
2007-10-09 10:26:02 +0000

    added sigint handler - fixes hangup on ctrl+c

--- pida/core/signalhandler.py
+++ pida/core/signalhandler.py
@@ -11,8 +11,12 @@ class PosixSignalHandler(object):
     def __init__(self, boss):
         self.boss = boss
         signal.signal(signal.SIGTERM, self.handle_SIGTERM)
+        signal.signal(signal.SIGINT, self.handle_SIGINT)
 
     def handle_SIGTERM(self, signum, frame):
         self.boss.log.error(_('PIDA stopped by SIGTERM')) 
         self.boss.stop(force=True)
 
+    def handle_SIGINT(self, signum, frame):
+        self.boss.log.info(_('PIDA stopped by SIGINT')) 
+        self.boss.stop(force=True)
