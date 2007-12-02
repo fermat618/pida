48311aa7efcf5eda0ef3458aa8bd70164f930f5e
alaxys <devnull@localhost>
2007-12-02 16:53:03 +0000

    we raise an exception if we couldn't load the file.

--- pida/editors/mooedit/mooedit.py
+++ pida/editors/mooedit/mooedit.py
@@ -365,8 +365,7 @@ class Mooedit(EditorService):
                     self.get_action('mooedit_find_word_prev').set_sensitive(True)
                     self.get_action('mooedit_replace').set_sensitive(True)
             else:
-                pass
-                #EA: the file could NOT be opened
+                raise Exception, 'Moo could not load the file.'
         else:
             #EA: the file was already open. we switch to it.
             self._embed.set_current_page(self._embed.page_num(self._documents[document.unique_id]))
