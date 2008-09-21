26f3f45c47639a1579b1ca14c701d25959bf69f3
Daniel Poelzleithner <poelzi@poelzi.org>
2008-09-21 23:04:04 +0200

    fix typo in buffer loading error handling

--- pida/services/buffer/buffer.py
+++ pida/services/buffer/buffer.py
@@ -406,7 +406,7 @@ class Buffer(Service):
             except DocumentException, e:
                 # document can't be loaded. we have to remove the document from 
                 # the system
-                self._recover_loading_error(e)
+                self.recover_loading_error(e)
             self.emit('document-changed', document=document)
         if line is not None:
             self.boss.editor.goto_line(line)
