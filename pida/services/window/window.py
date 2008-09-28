6ecdfe5caf8b0b21354c0715436ffac66aed4082
Daniel Poelzleithner <poelzi@poelzi.org>
2008-09-28 20:45:22 +0200

    windows: allow to configure the window title and better default one

--- pida/services/window/window.py
+++ pida/services/window/window.py
@@ -21,6 +21,7 @@
 #SOFTWARE.
 
 import gtk
+import string
 
 # PIDA Imports
 from pida.core.service import Service
@@ -30,6 +31,7 @@ from pida.core.options import OptionsConfig, Color
 from pida.core.actions import ActionsConfig
 from pida.core.actions import TYPE_NORMAL, TYPE_TOGGLE
 from pida.core.document import Document
+from pida.core.environment import session_name
 
 # locale
 from pida.core.locale import Locale
@@ -123,7 +125,7 @@ class WindowActionsConfig(ActionsConfig):
         getattr(self.svc, action.get_name())(val)
 
 class WindowEvents(EventsConfig):
-    
+
     def subscribe_all_foreign(self):
         self.subscribe_foreign('buffer', 'document-changed',
             self.on_document_changed)
@@ -131,11 +133,7 @@ class WindowEvents(EventsConfig):
             self.on_editor_started)
 
     def on_document_changed(self, document):
-        if document.is_new:
-            self.svc.window.set_title(_("New Document"))
-        else:
-            self.svc.window.set_title(document.filename)
-            
+        self.svc.update_title(document=document)
 
     def on_editor_started(self):
         self.svc.boss.hide_splash()
@@ -161,7 +159,19 @@ class WindowOptionsConfig(OptionsConfig):
             _('Whether the main menubar will be shown'),
             self.on_show_ui,
         )
-        
+
+        self.create_option(
+            'window_title',
+            _('Window title'),
+            str,
+            'Pida - $session - $filename',
+            _('Title template for the pida window.\n'
+              '$basename : Filename of Document - $filepath : Full filepath \n'
+              '$directory : Directory if file - $session : Session name \n'
+              '$project_path - $project_name'),
+            self.on_title_change,
+        )
+
         self.create_option(
             'project_color',
             _('Project color'),
@@ -186,6 +196,10 @@ class WindowOptionsConfig(OptionsConfig):
 
     def on_color_change(self, client, id, entry, option):
         self.svc.update_colors()
+        
+    def on_title_change(self, client, id, entry, option):
+        self.svc._title_template = None
+        self.svc.update_title()
 
 # Service class
 class Window(Service):
@@ -197,6 +211,7 @@ class Window(Service):
     events_config = WindowEvents
     
     def pre_start(self):
+        self._title_template = None
         super(Window, self).pre_start()
         self.update_colors()
 
@@ -212,6 +227,23 @@ class Window(Service):
         # set the colors of Document
         Document.markup_directory_color = self.opt('directory_color')
         Document.markup_project_color = self.opt('project_color')
+
+    def update_title(self, document=None):
+        if self._title_template is None:
+            self._title_template = string.Template(self.opt('window_title'))
+        if document is None:
+            document = self.boss.cmd('buffer', 'get_current')
+        
+        subs = {'basename': document.basename or _('New Document'),
+                'filepath': document.filename or _('New Document'),
+                'directory': document.directory or '',
+                'session': session_name(),
+                'project_path': document.project and document.project.data_dir or '',
+                'project_name': document.project_name
+               }
+        
+        self.window.set_title(self._title_template.safe_substitute(subs))
+        
 
     def _fix_visibilities(self):
         for name in ['show_toolbar', 'show_menubar']:
