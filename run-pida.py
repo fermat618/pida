d1c98405da7a2b3c647e3463ac263644f9c6c919
Daniel Poelzleithner <poelzi@poelzi.org>
2008-10-02 20:23:16 +0200

    add forgotten PIDA_HOME env

--- run-pida.py
+++ run-pida.py
@@ -5,7 +5,9 @@ import os
 import sys
 path = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, path)
-from pida.core.application import main
-main()
+from pida.core import application
+os.environ['PIDA_PATH'] = os.path.dirname(os.path.dirname(
+                                os.path.abspath(application.__file__)))
+application.main()
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
