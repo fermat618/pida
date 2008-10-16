09e2e6b6f4e9846ccebf5cc3495e0ec830106af7
Daniel Poelzleithner <poelzi@poelzi.org>
2008-10-16 03:28:34 +0200

    fixed copyright on new files

--- pida/utils/addtypes.py
+++ pida/utils/addtypes.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    :copyright: 2005-2008 by The PIDA Project
+    :license: GPL 2 or later (see README/COPYING/LICENSE)
+"""
+
 import types, exceptions
 
 class EnumException(exceptions.Exception):
