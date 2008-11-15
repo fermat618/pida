68438a422b2c74e5b6780dc3b3a7e7385bff2442
Ali Afshar <aafshar@gmail.com>
2008-11-15 14:20:13 +0000

    more puilder tests

--- pida/utils/puilder/tests/test_model.py
+++ pida/utils/puilder/tests/test_model.py
@@ -20,12 +20,14 @@ def _get_test_build():
     json = dumps(t, sort_keys=False, indent=2)
     return Build.loads(json), json
 
+
 def _build_test(f):
     b, j = _get_test_build()
     def _f():
         return f(b)
     _f.__name__ = f.__name__
     return _f
+
 
 def _target_test(f):
     b, j = _get_test_build()
@@ -34,6 +36,7 @@ def _target_test(f):
         return f(t)
     _f.__name__ = f.__name__
     return _f
+
 
 def _action_test(f):
     b, j = _get_test_build()
@@ -49,23 +52,39 @@ def _action_test(f):
 def test_targets(b):
     assert len(b.targets) == 1
 
+
 @_target_test
 def test_target_name(t):
     assert t.name == 'test'
+
 
 @_target_test
 def test_target_actions(t):
     assert(len(t.actions) == 1)
 
+
+@_target_test
+def test_target_serialise(t):
+    assert t.for_serialize() == {'name':u'test',
+        'actions':[{'type':u'shell','value':u'blah','options':{}}]}
+
+
 @_action_test
 def test_action_type(a):
     assert a.type == 'shell'
+
 
 @_action_test
 def test_action_value(a):
     assert a.value == 'blah'
 
+
 @_action_test
 def test_action_options(a):
     assert a.options == {}
+
+
+@_action_test
+def test_action_serialize(a):
+    assert a.for_serialize() == {'type':u'shell','value':u'blah','options':{}}
 
