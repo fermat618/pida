fd4de20899737fbd4902e99f3c0fc9fa7d94975b
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2008-12-15 02:06:27 +0100

    fixed various threading issues in filemanager

--- pida/services/filemanager/filemanager.py
+++ pida/services/filemanager/filemanager.py
@@ -369,7 +369,8 @@ class FilemanagerView(PidaView):
 
     def _get_ancestors(self, directory):
         ancs = [directory]
-        while directory != '/':
+        parent = None
+        while directory != parent:
             parent = os.path.dirname(directory)
             ancs.append(parent)
             directory = parent
@@ -466,19 +467,24 @@ class FilemanagerView(PidaView):
                                                            is not None)
 
     def paste_clipboard(self):
-        task = AsyncTask(self._paste_clipboard, lambda: None)
-        task.start()
-
-    def _paste_clipboard(self):
         newname = os.path.join(self.path, os.path.basename(self._clipboard_file))
         if newname == self._clipboard_file:
             self.svc.error_dlg(_('Cannot copy files to themselves.'))
             return
         if not os.path.exists(self._clipboard_file):
             self.svc.error_dlg(_('Source file has vanished.'))
+            return
         if os.path.exists(newname):
             self.svc.error_dlg(_('Destination already exists.'))
             return
+        
+        task = AsyncTask(self._paste_clipboard, lambda: None)
+        task.start()
+
+    def _paste_clipboard(self):
+        #XXX: in thread
+        newname = os.path.join(self.path, os.path.basename(self._clipboard_file))
+        #XXX: GIO?
         if os.path.isdir(self._clipboard_file):
             shutil.copytree(self._clipboard_file, newname)
         else:
@@ -495,7 +501,7 @@ class FilemanagerView(PidaView):
             os.remove(path)
         if path == self._clipboard_file:
             self._clipboard_file = None
-            self._fix_paste_sensitivity()
+            gcall(self._fix_paste_sensitivity)
 
 class FilemanagerEvents(EventsConfig):
 
