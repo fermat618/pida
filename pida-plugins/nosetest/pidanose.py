d29e364e5995c7edb737e240bc63c0bf7690bdf8
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2009-03-20 22:19:13 +0100

    port nosetest runner to dbus reporting
    this is still a mess tho, need to get better background commanders

--- pida-plugins/nosetest/pidanose.py
+++ pida-plugins/nosetest/pidanose.py
@@ -1,13 +1,74 @@
 #!/usr/bin/env python
-import sys
+import sys, os
 
 import nose
 from nose.core import TestProgram
 from nose.config import Config, all_config_files
+from nose.plugins import Plugin
 from nose.plugins.manager import PluginManager, DefaultPluginManager, \
      RestrictedPluginManager
-from xmlplugin.xmlplugin import XmlOutput
+import traceback
 ### configure paths, etc here
+import dbus
+
+class DBusReport(Plugin):
+
+    name = 'dbus-reporter'
+    score = 2 # run late
+
+    def __init__(self):
+        super(DBusReport, self).__init__()
+        bus = dbus.SessionBus()
+        object = bus.get_object(
+            'uk.co.pida.pida.'+os.environ['PIDA_DBUS_UUID'],
+            '/uk/co/pida/pida/nosetest'
+            )
+        self.proxy = dbus.Interface(object, 'uk.co.pida.pida.nosetest')
+        self.proxy.beginProcess(os.getcwd())
+
+
+    def addSuccess(self, test):
+        description = test.shortDescription() or str(test)
+        self.proxy.addSuccess(description)
+
+    def addError(self, test, err):
+        err = self.formatErr(err)
+        description = test.shortDescription() or str(test)
+        self.proxy.addError(description, err)
+
+    def addFailure(self, test, err):
+        err = self.formatErr(err)
+        description = test.shortDescription() or str(test)
+        self.proxy.addFailure(description, err)
+
+    def finalize(self, result):
+        self.proxy.endProcess()
+
+    def formatErr(self, err):
+        exctype, value, tb = err
+        return ''.join(traceback.format_exception(exctype, value, tb))
+
+    def startContext(self, ctx):
+        try:
+            n = ctx.__name__
+        except AttributeError:
+            n = str(ctx)
+        try:
+            path = ctx.__file__.replace('.pyc', '.py')
+        except AttributeError:
+            path = ''
+        self.proxy.startContext(n, path)
+
+    def stopContext(self, ctx):
+        self.proxy.stopContext()
+
+    def startTest(self, test):
+        description = test.shortDescription() or str(test)
+        self.proxy.startTest(description)
+
+    def stopTest(self, test):
+        self.proxy.stopTest()
+
 
 class PidaTest(TestProgram):
     def makeConfig(self, env, plugins=None):
@@ -19,7 +80,7 @@ class PidaTest(TestProgram):
             manager = PluginManager(plugins=plugins)
         else:
             manager = DefaultPluginManager()
-        manager.addPlugin(XmlOutput())
+        manager.addPlugin(DBusReport())
         return Config(
             env=env, files=cfg_files, plugins=manager)
 
