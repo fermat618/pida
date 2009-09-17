f53c2c1651ba2b79e0e2c0e66e6ef3a0a37b44cd
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2009-09-17 16:04:04 +0200

    glade starter accepts arguments now

--- tools/run-glade3.sh
+++ tools/run-glade3.sh
@@ -8,4 +8,4 @@ echo $SCRIPTPATH
 PYTHONPATH=$SCRIPTPATH
 GLADE_CATALOG_PATH=$SCRIPTPATH/glade3-plugin/ \
 GLADE_MODULE_PATH=$SCRIPTPATH/glade3-plugin/  \
-PYTHONPATH=$SCRIPTPATH/glade3-plugin/:$SCRIPTPATH/../externals/ glade-3
+PYTHONPATH=$SCRIPTPATH/glade3-plugin/:$SCRIPTPATH/../externals/ glade-3 $*
