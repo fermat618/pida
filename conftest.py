59c66ba4b4113ec9eb24f13a6d91211beabcdb87
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2009-09-28 11:29:12 +0200

    remove some conftest stuff that did hide the vim tests

--- conftest.py
+++ conftest.py
@@ -7,15 +7,7 @@ from pida.utils.testing.mock import Mock
 
 collect_ignore = ['tools/skeleton']
 
-class PidaClassCollect(py.test.collect.Class):
-    def collect(self):
-        # ignore classes that want params
-        if not hasattr(self.obj, '__init__'):
-            return super(PidaClassCollect, self).collect()
-
-
 class Module(py.test.collect.Module):
-    Class = PidaClassCollect
 
     def makeitem(self, name, obj):
         if isinstance(obj, Mock):
