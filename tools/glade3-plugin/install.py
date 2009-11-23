190a7a3136170b8fe29459d3f5ff52dca8917e99
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2009-11-23 21:31:18 +0100

    clean up glade plugin install script, fixes #352

--- tools/glade3-plugin/install.py
+++ tools/glade3-plugin/install.py
@@ -1,17 +1,26 @@
-from os.path import join
-import subprocess, sys, os
 
-pipe = subprocess.Popen(['which', 'glade-3'], stdout=subprocess.PIPE)
-sto = pipe.communicate()[0]
-if pipe.returncode:
+from os.path import join, dirname
+import os
+from shutil import copy
+
+found = False
+
+for path in os.environ['PATH'].split(os.pathsep):
+    path = join(path, 'glade-3')
+    if os.access(path, os.X_OK):
+        found = True
+        break
+
+if not found:
     print "glade-3 not found in path"
+    raise SystemExit
 
-glade3_prefix = sto.strip()[:len('bin/glade-3')-1]
+glade3_prefix = dirname(dirname(path))
 
-print "installing in prefix %s" %glade3_prefix
+print "installing in prefix", glade3_prefix
 
 
-subprocess.call(["cp", 'kiwiwidgets.xml', 
-          os.path.join(glade3_prefix, 'share', 'glade3', 'catalogs')])
-subprocess.call(["cp", 'kiwiwidgets.py',
-          os.path.join(glade3_prefix, 'lib', 'glade3', 'modules')])
+copy('kiwiwidgets.xml',
+     os.path.join(glade3_prefix, 'share/glade3/catalogs'))
+copy('kiwiwidgets.py',
+     os.path.join(glade3_prefix, 'lib/glade3/modules'))
