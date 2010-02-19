4c1177232f1c93e499d699790bab9e7d9699bdcd
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-02-19 11:14:07 +0100

    clean up and fix project tests

--- tests/core/test_project.py
+++ tests/core/test_project.py
@@ -25,285 +25,247 @@ def rmdir(top):
 
 pf = lambda *x: os.path.sep.join(x)
 
+def make_project_files(tmpdir):
+    dirs = [
+            'src',
+            'lib',
+            '.SVN',
+            'src/.SVN',
+            'src/CVS',
+            'src/test2',
+            'lib/bla',
+            'lib/CVS'
+            ]
+    for dir in dirs:
+        tmpdir.ensure(dir, dir=True)
 
-class ProjectTest(TestCase):
+    files = [
+        'src/source.c',
+        'src/source2.c',
+        'src/source2.h',
+        'src/skript.sh',
+        'src/Makefile',
+        'lib/Makefile',
+        'lib/readme',
+        'lib/bla/readme',
+        'LICENSE',
+        '.hiddenfile',
+    ]
+    for file in files:
+        tmpdir.ensure(file)
 
-    def setUp(self):
-        self.ppath = mkdtemp('pidatest')
-        Project.create_blank_project_file('test', self.ppath)
-        self.project = Project(self.ppath)
+def pytest_funcarg__project(request):
+    tmpdir = request.getfuncargvalue('tmpdir')
+    Project.create_blank_project_file('test', str(tmpdir))
+    return Project(str(tmpdir))
 
-    def tearDown(self):
-        # clean up temp directory
-        rmdir(self.ppath)
+def test_meta_dir(project, tmpdir):
+    m1 = project.get_meta_dir()
+    assert m1 == tmpdir.join(DATA_DIR)
+    assert os.path.exists(m1)
+    m2 = project.get_meta_dir('plug1')
+    assert m2 == tmpdir.join(DATA_DIR, 'plug1')
+    assert os.path.exists(m2)
 
-    def test_meta_dir(self):
-        ppath = self.ppath
-        p = Project(ppath)
-        m1 = p.get_meta_dir()
-        self.assertEqual(m1, os.path.join(ppath, DATA_DIR))
-        self.assertEqual(os.path.exists(m1), True)
-        m2 = p.get_meta_dir('plug1')
-        self.assertEqual(m2, os.path.join(ppath, DATA_DIR, 'plug1'))
-        self.assertEqual(os.path.exists(m2), True)
-    
-    def test_relpath(self):
-        self.assertEqual(self.project.get_relative_path_for(
-            os.path.join(self.ppath, "something", "else.py")),
-            ["something", "else.py"]
-            )
-        filename = os.path.join(self.ppath, "module", "mod2.py")
-        self.assertEqual(
-            ".".join(self.project.get_relative_path_for(filename))[:-3],
-            'module.mod2'
-            )
-        self.assertEqual(self.project.get_relative_path_for(
-                                self.project.source_directory), 
-                         [])
+def test_relpath(project, tmpdir):
+    check = str(tmpdir.join('something','else.py'))
+    relative = project.get_relative_path_for(check)
+    assert relative == ["something", "else.py"]
 
-    def make_project_files(self):
-        pp = partial(os.path.join, self.project.source_directory)
-
-        os.mkdir(pp(".SVN"))
-        os.mkdir(pp("src", ".SVN"))
-        os.mkdir(pp("src", "CVS"))
-        os.mkdir(pp("src", "test2"))
-        os.mkdir(pp("lib", "bla"))
-        os.mkdir(pp("lib", "CVS"))
-        mkfile(pp("src", "source.c"))
-        mkfile(pp("src", "source2.c"))
-        mkfile(pp("src", "source2.h"))
-        mkfile(pp("src", "skrip.sh"))
-        mkfile(pp("src", "Makefile"))
-        mkfile(pp("lib", "Makefile"))
-        mkfile(pp("lib", "readme"))
-        mkfile(pp("lib", "bla", "readme"))
-        mkfile(pp("LICENSE"))
-        mkfile(pp(".hiddenfile"))
+    assert project.get_relative_path_for(project.source_directory) == []
 
 
-    def test_cache(self):
-        self.project.load_cache()
-        #the empty cache should contain the root elements and the metadata
-        self.assertEqual(len(self.project._cache['dirs']), 2)
-        self.assertEqual(len(self.project._cache['paths']), 3)
-        self.assertEqual(len(self.project._cache['dirnames']), 2)
-        self.assertEqual(len(self.project._cache['files']), 1)
-        self.assertEqual(len(self.project._cache['filenames']), 1)
 
 
-        pp = partial(os.path.join, self.project.source_directory)
+def test_cache(project, tmpdir):
+    project.load_cache()
+    project.index('', recrusive=True) #XXX: added cause of fucked cache
 
-        os.mkdir(pp("src"))
-        os.mkdir(pp("lib"))
-        self.project.index("", recrusive=True)
-        self.assertTrue(
-            all([x in self.project._cache['dirnames'] for x in 
-                ['src', 'lib', '.pida-metadata']]))
-        self.assertTrue(
-            all([x in self.project._cache['dirs'] for x in 
-                ['src', 'lib', '.pida-metadata']]))
+    c = project._cache
+    #the empty cache should contain the root elements and the metadata
+    assert len(c['dirs']) == 2
+    assert len(c['paths']) == 3
+    assert len(c['dirnames']) == 2
+    assert len(c['files']) == 1
+    assert len(c['filenames']) ==1
 
-        os.mkdir(pp("src", "test"))
-        os.mkdir(pp("outside"))
-        self.project.index("src", recrusive=True)
+    tmpdir.ensure('src', dir=True)
+    tmpdir.ensure('lib', dir=True)
 
-        self.assertTrue(
-            all([x in self.project._cache['dirnames'] for x in 
-                ['src', 'lib', '.pida-metadata', 'test']]))
-        self.assertTrue(
-            all([x in self.project._cache['dirs'] for x in 
-                ['src', 'lib', '.pida-metadata', 'src/test']]))
+    project.index(recrusive=True)
+    assert sorted(c['dirnames']) == ['', '.pida-metadata', 'lib', 'src']
+    assert sorted(c['dirs']) == ['', '.pida-metadata', 'lib', 'src']
 
-        self.assertTrue(not 'outside' in self.project._cache['dirs'])
-        self.assertTrue(not 'outside' in self.project._cache['dirnames'])
+    tmpdir.ensure("src", "test", dir=True)
+    tmpdir.ensure("outside", dir=True)
+    project.index("src", recrusive=True)
 
-        #generate dummy data
-        self.make_project_files()
+    print sorted(c['dirs'])
+    assert sorted(c['dirnames']) == ['', '.pida-metadata', 'lib', 'src', 'test']
+    assert sorted(c['dirs']) == ['', '.pida-metadata', 'lib', 'src', 'src/test']
 
-        self.project.index("", recrusive=True)
+    assert 'outside' not in c['dirs']
+    assert 'outside' not in c['dirnames']
 
-        # test doctypes
-        self.assertEqual(self.project._cache['files']['LICENSE'].doctype,
-                         None)
-        self.assertEqual(self.project._cache['files'][
-                                    pf('src', 'source2.c')].doctype,
-                         'C')
-        self.assertEqual(self.project._cache['files'][
-                                    pf('src', 'skrip.sh')].doctype,
-                         'Bash')
+    #generate dummy data
+    make_project_files(tmpdir)
+    project.index("", recrusive=True)
 
-        self.assertEqual(len(self.project._cache['filenames']['readme']), 2)
-        self.assertEqual(len(self.project._cache['filenames']['Makefile']), 2)
-        self.assertEqual(len(self.project._cache['dirnames']['src']), 1)
-        self.assertEqual(len(self.project._cache['dirnames']['CVS']), 2)
+    # test doctypes
+    assert c['files']['LICENSE'].doctype is None
+    assert c['files']['src/source2.c'].doctype == 'C'
+    assert c['files']['src/skript.sh'].doctype == 'Bash'
+
+    assert len(c['filenames']['readme']) == 2
+    assert len(c['filenames']['Makefile']) == 2
+    assert len(c['dirnames']['src']) == 1
+    assert len(c['dirnames']['CVS']) == 2
 
 
-        # start removing files
-        rmdir(pp('lib', 'bla'))
-        self.assertTrue(not os.path.exists(pp('lib', 'bla')))
-        self.project.index(pf('lib'), recrusive=True)
+    # start removing files
+    tmpdir.join('lib', 'bla').remove(rec=True)
+    assert not tmpdir.join('lib', 'bla').check()
+    project.index('lib', recrusive=True)
 
-        self.assertTrue(not 'bla' in self.project._cache['dirnames'])
-        self.assertTrue(not pf('bla') in self.project._cache['paths'])
-        self.assertEqual(len(self.project._cache['filenames']['readme']), 1)
-        self.assertEqual(len(self.project._cache['filenames']['Makefile']), 2)
-        self.assertEqual(len(self.project._cache['dirnames']['src']), 1)
-        self.assertEqual(len(self.project._cache['dirnames']['CVS']), 2)
+    assert 'bla' not in c['dirnames']
+    assert 'bla' not in c['paths']
+    assert len(c['filenames']['readme']) == 1
+    assert len(c['filenames']['Makefile']) == 2
+    assert len(c['dirnames']['src']) == 1
+    assert len(c['dirnames']['CVS']) == 2
 
-        dirs = [
-            pf(".SVN"),
-            pf("src", ".SVN"),
-            pf("src", "CVS"),
-            pf("src", "test2"),
-            pf("lib", "CVS")]
+    dirs = [
+        '.SVN',
+        'src/.SVN',
+        'src/CVS',
+        'src/test2',
+        'lib/CVS',
+    ]
 
-        files = [
-            pf("src", "source.c"),
-            pf("src", "source2.c"),
-            pf("src", "source2.h"),
-            pf("src", "skrip.sh"),
-            pf("src", "Makefile"),
-            pf("lib", "Makefile"),
-            pf("lib", "readme"),
-            pf("LICENSE"),
-            pf(".hiddenfile")]
+    files = [
+        'src/source.c',
+        'src/source2.c',
+        'src/source2.h',
+        'src/skript.sh',
+        'src/Makefile',
+        'lib/Makefile',
+        'lib/readme',
+        'LICENSE',
+        '.hiddenfile',
+        ]
+    print sorted(c['dirs'])
+    assert set(files).issubset(c['files'])
+    assert set(dirs).issubset(c['dirs'])
 
-        self.assertEqual(
-            all([x in self.project._cache['files'] for x in files]), True)
-        self.assertEqual(
-            all([x in self.project._cache['dirs'] for x in dirs]), True)
-        self.assertEqual(
-            all([x in self.project._cache['paths'] for x in files + dirs]),
-            True)
+    assert set(files).issubset(c['paths'])
+    assert set(dirs).issubset(c['paths'])
 
-        # save the state and reload
-        self.project.save_cache()
-        self.project.load_cache()
+    # save the state and reload
+    project.save_cache()
+    assert project.load_cache()
+    c = project._cache # loadin sets the cache
 
-        self.assertEqual(
-            all([x in self.project._cache['files'] for x in files]), True)
-        self.assertEqual(
-            all([x in self.project._cache['dirs'] for x in dirs]), True)
-        self.assertEqual(
-            all([x in self.project._cache['paths'] for x in files + dirs]),
-            True)
+    assert set(files).issubset(c['files'])
+    assert set(dirs).issubset(c['dirs'])
 
-        # test for equality of instances
-        self.assertEqual(self.project._cache['paths'][pf("src", "source2.h")], 
-                         self.project._cache['filenames']["source2.h"][0])
+    assert set(files).issubset(c['paths'])
+    assert set(dirs).issubset(c['paths'])
 
-        # add new file
-        mkfile(pp('src', 'blubb'))
-        os.mkdir(pp('src', 'noho'))
-        self.project.index_path(pp('src', 'blubb'))
-        self.project.index_path(pp('src', 'noho'))
-        self.assertEqual(len(self.project._cache['filenames']['blubb']), 1)
-        self.assertEqual(self.project._cache['files'][pf('src', 'blubb')].relpath,
-                            pf('src', 'blubb'))
-        
-        self.assertEqual(len(self.project._cache['dirnames']['noho']), 1)
-        self.assertEqual(self.project._cache['dirs'][pf('src', 'noho')].relpath,
-                            pf('src', 'noho'))
-        
+    # test for equality of instances
+    assert c['paths']['src/source2.h'] == c['filenames']["source2.h"][0]
 
-    def test_query(self):
-        pp = partial(os.path.join, self.project.source_directory)
+    # add new file
+    tmpdir.ensure('src/blubb')
+    tmpdir.ensure('src/noho', dir=True)
+    project.index('src/blubb')
+    project.index('src/noho')
+    assert len(c['filenames']['blubb']) == 1
+    assert c['files']['src/blubb'].relpath == 'src/blubb'
 
-        os.mkdir(pp("src"))
-        os.mkdir(pp("lib"))
-        self.make_project_files()
-
-        self.project.index(recrusive=True)
-
-        self.assertEqual(len(list(self.project.query_basename('source.c'))), 1)
-        self.assertEqual(list(self.project.query_basename('source.c'))[0], 
-                         self.project._cache['filenames']['source.c'][0])
-
-        # non existing through wrong case
-        self.assertEqual(len(list(self.project.query_basename('source.c2'))), 0)
-        self.assertEqual(len(list(self.project.query_basename('source.C'))), 0)
-        self.assertEqual(len(list(self.project.query_basename(''))), 0)
-
-        self.assertEqual(
-            len(list(self.project.query_basename('source.c2', dirs=True))), 0)
-        self.assertEqual(
-            len(list(self.project.query_basename('source.C', dirs=True))), 0)
-
-        # test for directories
-        self.assertEqual(len(list(self.project.query_basename('lib'))), 0)
-        self.assertEqual(
-            len(list(self.project.query_basename('lib', dirs=True))), 1)
-        self.assertEqual(
-            len(list(self.project.query_basename('liB', dirs=True))), 0)
-
-        self.assertEqual(
-           len(list(self.project.query_basename('lib', dirs=True, glob=True))),
-           1)
-        #globs
-        self.assertEqual(
-           len(list(self.project.query_basename('l??', dirs=True, glob=True))),
-           1)
-        self.assertEqual(
-            len(list(self.project.query_basename('source.*', glob=True))), 1)
-        self.assertEqual(
-            len(list(self.project.query_basename('source2.?', glob=True))), 2)
-        self.assertEqual(
-            len(list(self.project.query_basename('source*', glob=True))), 3)
-
-        rv = [x.relpath for x in self.project.query_basename(None)]
-        rv.sort()
-        self.assertEqual(rv, 
-                         ['.hiddenfile', '.pida-metadata/project.json', 
-                          'LICENSE', 'lib/Makefile', 'lib/bla/readme', 
-                          'lib/readme', 'src/Makefile', 'src/skrip.sh', 
-                          'src/source.c', 'src/source2.c', 'src/source2.h'])
+    assert len(c['dirnames']['noho']) == 1
+    assert c['dirs']['src/noho'].relpath == 'src/noho'
 
 
-        rv = [x.relpath for x in self.project.query_basename(None, dirs=True)]
-        rv.sort()
-        self.assertEqual(rv,
-                        ['', '.SVN', '.hiddenfile', '.pida-metadata', 
-                        '.pida-metadata/project.json', 'LICENSE', 'lib', 
-                        'lib/CVS', 'lib/Makefile', 'lib/bla', 'lib/bla/readme', 
-                        'lib/readme', 'src', 'src/.SVN', 'src/CVS', 
-                        'src/Makefile', 'src/skrip.sh', 'src/source.c', 
-                        'src/source2.c', 'src/source2.h', 'src/test2'])
+def test_query(project, tmpdir):
+    pp = partial(os.path.join, project.source_directory)
 
-        # test query interface
+    make_project_files(tmpdir)
 
-        # test for a filetype
-        def testc(info):
-            if info.doctype == "C":
-                return RESULT.YES
-            return RESULT.NO
+    project.index(recrusive=True)
+    def query(*k, **kw):
+        return [x.relpath for x in project.query_basename(*k,**kw)]
 
-        self.assertEqual([x.basename for x in 
-                          self.project.query(testc)],
-                         ['source.c', 'source2.c', 'source2.h'])
+    assert query('source.c') == ['src/source.c']
+    rel = query('source.c')[0]
+    assert rel== project._cache['filenames']['source.c'][0].relpath
 
-        # test subdir check
-        def testc(info):
-            if info.basename == "lib":
-                return RESULT.NO_NOCHILDS
-            elif info.is_dir:
-                return RESULT.YES
+    # non existing through wrong case
+    assert query('source.c2') == []
+    assert query('source.C') == []
+    assert query('') == []
 
-        self.assertEqual([x.relpath for x in 
-                          self.project.query(testc)],
-                         ['', '.SVN', '.pida-metadata', 'src', 'src/.SVN', 
-                          'src/CVS', 'src/test2'])
+    assert query('source.c2', dirs=True) == []
+    assert query('source.C', dirs=True) == []
 
-        def testc(info):
-            if info.basename == "lib":
-                return RESULT.YES_NOCHILDS
+    # test for directories
+    assert query('lib') == []
+    infos = query('lib', dirs=True) == ['lib']
+    assert query('liB', dirs=True) == []
+
+    assert query('lib', dirs=True, glob=True) == ['lib']
+    #globs
+    assert len(query('l??', dirs=True, glob=True)) == 1
+    assert len(query('source.*', glob=True)) == 1
+    assert len(query('source2.?', glob=True)) == 2
+    assert len(query('source*', glob=True)) == 3
+
+    rv =  sorted(query(None))
+    assert rv == ['.hiddenfile', '.pida-metadata/project.json',
+                  'LICENSE', 'lib/Makefile', 'lib/bla/readme',
+                  'lib/readme', 'src/Makefile', 'src/skript.sh',
+                  'src/source.c', 'src/source2.c', 'src/source2.h']
+    rv = sorted(query(None, dirs=True))
+    assert rv == ['', '.SVN', '.hiddenfile', '.pida-metadata',
+                  '.pida-metadata/project.json', 'LICENSE', 'lib',
+                  'lib/CVS', 'lib/Makefile', 'lib/bla', 'lib/bla/readme',
+                  'lib/readme', 'src', 'src/.SVN', 'src/CVS',
+                  'src/Makefile', 'src/skript.sh', 'src/source.c',
+                  'src/source2.c', 'src/source2.h', 'src/test2']
+
+    # test query interface
+
+    # test for a filetype
+    def testc(info):
+        if info.doctype == "C":
+            return RESULT.YES
+        return RESULT.NO
+
+    bases = [x.basename for x in project.query(testc)]
+    assert bases == ['source.c', 'source2.c', 'source2.h']
+
+    # test subdir check
+    def testc(info):
+        if info.basename == "lib":
+            return RESULT.NO_NOCHILDS
+        elif info.is_dir:
             return RESULT.YES
 
-        self.assertEqual([x.relpath for x in 
-                          self.project.query(testc)],
-                         ['', '.SVN', '.hiddenfile', '.pida-metadata', 
-                          '.pida-metadata/project.json', 'LICENSE', 'lib', 
-                          'src', 'src/.SVN', 'src/CVS', 'src/Makefile', 
-                          'src/skrip.sh', 'src/source.c', 'src/source2.c', 
-                          'src/source2.h', 'src/test2'])
+    bases = [x.relpath for x in project.query(testc)]
+    print bases
+
+    assert bases == ['', '.SVN', '.pida-metadata', 'src', 'src/.SVN',
+                     'src/CVS', 'src/test2']
+
+    def testc(info):
+        if info.basename == "lib":
+            return RESULT.YES_NOCHILDS
+        return RESULT.YES
+
+    bases = [x.relpath for x in project.query(testc)]
+    assert bases == ['', '.SVN', '.hiddenfile', '.pida-metadata',
+                    '.pida-metadata/project.json', 'LICENSE', 'lib',
+                    'src', 'src/.SVN', 'src/CVS', 'src/Makefile',
+                    'src/skript.sh', 'src/source.c', 'src/source2.c',
+                    'src/source2.h', 'src/test2',
+                    ]
 
