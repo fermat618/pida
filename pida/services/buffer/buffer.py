7a57ef268943af230d9b2145914e884ddb873c42
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-03-21 09:31:52 +0100

    fix issue in document loading error recovery

--- pida/services/buffer/buffer.py
+++ pida/services/buffer/buffer.py
@@ -479,7 +479,7 @@ class Buffer(Service):
         else:
             filename = ""
         self.notify_user(error.message, title=_("Can't load file %s") % filename )
-        self.log('error loading file(s): %s' %error.message)
+        self.log.warning('error loading file(s): %s' %error.message)
         if error.document:
             self._remove_document(error.document)
         # switch to the first doc to make sure editor gets consistent
