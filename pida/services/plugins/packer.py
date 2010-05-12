1bd443b63e33c7371c429eeb77310cc5f0f4e0f0
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-05-12 20:55:37 +0200

    use print as default notify for the plugin packer

--- pida/services/plugins/packer.py
+++ pida/services/plugins/packer.py
@@ -39,8 +39,10 @@ def find_files(base, service):
     paths.insert(1, service_file)
     return paths
 
+def _default_notify(text):
+    print text
 
-def pack_plugin(base, service, notify=lambda x:None):
+def pack_plugin(base, service, notify=_default_notify):
     service_file = path.join(base, service, 'service.pida')
     base_len = len(base)
     if not path.exists(service_file):
