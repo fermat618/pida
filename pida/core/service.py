f5ded4d8287c7c93a78ac396a1551f736237c89f
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-07-15 16:05:17 +0200

    imply plugin type by the namespace

--- pida/core/service.py
+++ pida/core/service.py
@@ -62,11 +62,10 @@ class Service(object):
         self.actions.subscribe_keyboard_shortcuts()
 
     def __repr__(self):
-        #XXX: bad factoring, get better types
-        if self.__class__.__module__.startswith('pida.service'):
-            return '<Service: %s>' % self.__class__.__name__
-        else:
-            return '<Plugin: %s>' % self.__class__.__name__
+        return '<{kind}: {name}>'.format(
+            kind=self.__class__.__module__.split('.')[1].capitalize().rstrip('s'),
+            name=self.__class__.__name__,
+        )
 
     @classmethod
     def get_name(cls):
