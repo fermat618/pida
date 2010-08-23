f5b8c5df709efce6e425a29a076ea70534809376
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-08-23 12:21:31 +0200

    update hgdistver

--- hgdistver.py
+++ hgdistver.py
@@ -1,68 +1,102 @@
 import os
-import commands
+import sys
 import subprocess
+def getoutput(cmd, cwd='.'):
+    p = subprocess.Popen(cmd,
+                         shell=True,
+                         stdout=subprocess.PIPE,
+                         cwd=cwd,
+                        )
+    out, _ = p.communicate()
+    return out.decode() # will kill us sometimes
 
-def version_from_cachefile(cachefile=None):
+
+def hg(args, cwd='.'):
+    return getoutput('hg ' + args, cwd).strip()
+
+def version_from_cachefile(root, cachefile=None):
+    #XXX: for now we ignore root
     if not cachefile:
         return
     #replaces 'with open()' from py2.6
     fd = open(cachefile)
-    fd.readline() # remove the comment
+    fd.readline()  # remove the comment
     version = None
     try:
         line = fd.readline()
         version_string = line.split(' = ')[1].strip()
-        version = version_string[1:-1].decode('string-escape')
-    except: # any error means invalid cachefile
+        version = version_string[1:-1]
+    except:  # any error means invalid cachefile
         pass
     fd.close()
     return version
 
-def version_from_hg_id(cachefile=None):
+
+def version_from_hg_id(root, cachefile=None):
     """stolen logic from mercurials setup.py as well"""
-    if os.path.isdir('.hg'):
-        l = commands.getoutput('hg id -i -t').strip().split()
-        while len(l) > 1 and l[-1][0].isalpha(): # remove non-numbered tags
-            l.pop()
-        if len(l) > 1: # tag found
-            version = l[-1]
-            if l[0].endswith('+'): # propagate the dirty status to the tag
+    l = hg('id -i -t', root).split()
+    node = l.pop(0)
+    for tag in l:
+        #XXX: find better guess if version-number logic
+        if tag[0].isdigit():
+            version = tag
+            if node[-1] == '+':  # propagate the dirty status to the tag
                 version += '+'
             return version
 
-def version_from_hg15_parents(cachefile=None):
-    if os.path.isdir('.hg'):
-        hgver = commands.getoutput('python -c "import mercurial.__version__;print mercurial.__version__.version"')
-        if hgver < 1.5:
-            return version_from_hg_log_with_tags()
-        node = commands.getoutput('hg id -i')
-        if node == '000000000000+':
-            return '0.0.dev0-' + node
 
-        cmd = 'hg parents --template "{latesttag} {latesttagdistance}"'
-        out = commands.getoutput(cmd)
-        try:
-            tag, dist = out.split()
-            if tag=='null':
-                tag = '0.0'
-            return '%s.dev%s-%s' % (tag, dist, node)
-        except ValueError:
-            pass # unpacking failed, old hg
+def version_from_hg15_parents(root, cachefile=None):
+    node = hg('id -i', root)
+    if node.strip('+') == '000000000000':
+        return '0.0.dev0-' + node
 
-def version_from_hg_log_with_tags(cachefile=None):
-    if os.path.isdir('.hg'):
-        node = commands.getoutput('hg id -i')
-        cmd = r'hg log -r %s:0 --template "{tags}\n"'
-        cmd = cmd % node.rstrip('+')
-        proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-        dist = -1 # no revs vs one rev is tricky
+    cmd = 'parents --template "{latesttag} {latesttagdistance}"'
+    out = hg(cmd, root)
+    try:
+        tag, dist = out.split()
+        if tag == 'null':
+            tag = '0.0'
+        return '%s.dev%s-%s' % (tag, dist, node)
+    except ValueError:
+        pass  # unpacking failed, old hg
 
-        for dist, line in enumerate(proc.stdout):
-            tags = [t for t in line.split() if not t.isalpha()]
-            if tags:
-                return '%s.dev%s-%s' % (tags[0], dist, node)
 
-        return  '0.0.dev%s-%s' % (dist+1, node)
+def version_from_hg_log_with_tags(root, cachefile=None):
+    #NOTE: this is only a fallback called from version_from_hg15_parents
+    node = hg('id -i', root)
+    cmd = r'hg log -r %s:0 --template "{tags} \n"'
+    cmd = cmd % node.rstrip('+')
+    proc = subprocess.Popen(cmd,
+                            cwd=root,
+                            shell=True,
+                            stdout=subprocess.PIPE,
+                           )
+    dist = -1  # no revs vs one rev is tricky
+
+    for dist, line in enumerate(proc.stdout):
+        line = line.decode()
+        tags = [t for t in line.split() if not t.isalpha()]
+        if tags:
+            return '%s.dev%s-%s' % (tags[0], dist, node)
+
+    return  '0.0.dev%s-%s' % (dist + 1, node)
+
+
+def version_from_hg(root, cachefile=None):
+    # no .hg means no way to get it
+    if not os.path.isdir(os.path.join(root, '.hg')):
+        return
+    # if id has a tag we are lucky
+    version_from_id = version_from_hg_id(root)
+    if version_from_id:
+        return version_from_id
+    hgver_out = hg('--version')
+    hgver_out = hgver_out.splitlines()[0].rstrip(')')
+    hgver = hgver_out.split('version ')[-1]
+    if hgver < '1.5':
+        return version_from_hg_log_with_tags(root)
+    else:
+        return version_from_hg15_parents(root)
 
 def _archival_to_version(data):
     """stolen logic from mercurials setup.py"""
@@ -73,23 +107,29 @@ def _archival_to_version(data):
     else:
         return data.get('node', '')[:12]
 
+
 def _data_from_archival(path):
     import email
     data = email.message_from_file(open(str(path)))
     return dict(data.items())
 
-def version_from_archival(cachefile=None):
-    #XXX: asumes cwd is repo root
-    if os.path.exists('.hg_archival.txt'):
-        data = _data_from_archival('.hg_archival.txt')
-        return _archival_to_version(data)
 
-def version_from_sdist_pkginfo(cachefile=None):
-    if cachefile is None and os.path.exists('PKG-INFO'):
-        data = _data_from_archival('PKG-INFO')
+def version_from_archival(root, cachefile=None):
+    for parent in root, os.path.dirname(root):
+        archival = os.path.join(parent, '.hg_archival.txt')
+        if os.path.exists(archival):
+            data = _data_from_archival(archival)
+            return _archival_to_version(data)
+
+
+def version_from_sdist_pkginfo(root, cachefile=None):
+    pkginfo = os.path.join(root, 'PKG-INFO')
+    if cachefile is None and os.path.exists(pkginfo):
+        data = _data_from_archival(pkginfo)
         version = data.get('Version')
         if version != 'UNKNOWN':
             return version
+
 
 def write_cachefile(path, version):
     fd = open(path, 'w')
@@ -101,19 +141,22 @@ def write_cachefile(path, version):
 
 
 methods = [
-    version_from_hg_id,
-    version_from_hg15_parents,
-    version_from_hg_log_with_tags,
-    version_from_archival,
+    version_from_hg,
     version_from_cachefile,
     version_from_sdist_pkginfo,
+    version_from_archival,
 ]
 
-def get_version(cachefile=None):
+
+def get_version(cachefile=None, root=None):
+    if root is None:
+        root = os.getcwd()
+    if cachefile is not None:
+        cachefile = os.path.join(root, cachefile)
     try:
         version = None
         for method in methods:
-            version = method(cachefile=cachefile)
+            version = method(root=root, cachefile=cachefile)
             if version:
                 if version.endswith('+'):
                     import time
@@ -122,3 +165,7 @@ def get_version(cachefile=None):
     finally:
         if cachefile and version:
             write_cachefile(cachefile, version)
+
+
+if __name__ == '__main__':
+    print('Guessed Version %s' % (get_version(),))
