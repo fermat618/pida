043845882901900c8673fe0473873ee3e65c3738
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2011-03-25 13:25:02 +0100

    pytest-ify and fix the ctags tests

--- pida-plugins/ctags/test_ctags.py
+++ pida-plugins/ctags/test_ctags.py
@@ -20,7 +20,7 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-import unittest
+import py
 from ctags import CtagsOutliner, build_language_list
 from pida.core.document import Document
 import os
@@ -29,40 +29,42 @@ TESTDIR = os.path.join(os.path.dirname(__file__), 'test')
 
 from pida.utils.testing.mock import Mock
 
-class TestCtags(unittest.TestCase):
-    def test_parser(self):
-        self.doc = Document(None, os.path.join(TESTDIR, 'test.py'))
-        self.outliner = CtagsOutliner(None, document=self.doc)
-        lst = list(self.outliner.run())
-        outer = None
-        inner = None
-        for x in lst:
-            if x.name == "Outer":
-                outer = x
-            elif x.name == "Inner":
-                inner = x
-        self.assert_(outer)
-        self.assertEqual(outer.parent, None)
-        self.assertEqual(inner.parent, outer)
-        for x in lst:
-            if x.name[:6] == "outer_":
-                self.assertEqual(x.parent, outer)
-            if x.name[:6] == "inner_":
-                self.assertEqual(x.parent, inner)
-            #print x
+def test_parser():
+    if not py.path.local.sysfind('ctags'):
+        py.test.skip('ctags missing')
+    doc = Document(None, os.path.join(TESTDIR, 'test.py'))
+    outliner = CtagsOutliner(None, document=doc)
+    lst = list(outliner.run())
+    print lst
+    outer = None
+    inner = None
+    for x in lst:
+        if x.name == "Outer":
+            outer = x
+        elif x.name == "Inner":
+            inner = x
+    assert outer is not None
+    assert outer.parent is None
+    assert inner.parent is outer
+    for x in lst:
+        if x.name[:6] == "outer_":
+            assert x.parent is outer
+        if x.name[:6] == "inner_":
+            assert x.parent is inner
+        print x
 
-    def test_languagemap(self):
-        from pida.core.doctype import TypeManager
-        doctypes = TypeManager()
-        from pida.services.language import deflang
-        doctypes._parse_map(deflang.DEFMAPPING)
-        lst = build_language_list(doctypes)
-        if len(lst) == 0:
-            #FIXME: no ctags i guess, should be tested
-            return 
-        # those should be in the ctags
-        self.assertTrue(all([x in doctypes.values() for x in lst]))
-        self.assertTrue(len(lst) > 10)
+def test_languagemap():
+    from pida.core.doctype import TypeManager
+    doctypes = TypeManager()
+    from pida.services.language import deflang
+    doctypes._parse_map(deflang.DEFMAPPING)
+    lst = build_language_list(doctypes)
+    if len(lst) == 0:
+        #FIXME: no ctags i guess, should be tested
+        return 
+    # those should be in the ctags
+    assert all([x in doctypes.values() for x in lst])
+    assert len(lst) > 10
 
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
